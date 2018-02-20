5d5cb055d41b0dc85d82714590d03cd8de88f371
Nakul Kurane <nakurane@gmail.com>
2018-02-20 15:04:21 -0500

    adding some explanations for Pandas functions

--- 01_Getting_&_Knowing_Your_Data/Chipotle/chipo.py
+++ 01_Getting_&_Knowing_Your_Data/Chipotle/chipo.py
@@ -7,79 +7,77 @@ import pandas as pd
 # import tsv and assign to variable
 chipo = pd.read_csv('https://raw.githubusercontent.com/justmarkham/DAT8/master/data/chipotle.tsv', sep='\t', header=0)
 
-# print first ten records
+# EXERCISE: print first ten records
 print(chipo.head(10))
 
 # .info prints a dataframe of the columns (with names), the number of records in each column, and the data type of each column
 print(chipo.info())
 print('\n')
 
-# print num of observations in dataset
+# EXERCISE: print num of observations in dataset
+# .shape gets a tuple of the number of rows and the number of columns (in that order, so [0] will get the num of rows)
 print(chipo.shape[0])
 print('\n')
 
-# num of columns in dataset
+# EXERCISE: num of columns in dataset
+# .columns returns a series (aka an array) of the column names
+# len() gets the length of the series
+# str() converts the value in the parameter to a string
 c = str(len(chipo.columns))
 col = 'This dataset has ' + c + ' columns'
 
 print(col)
 
-# prints list of column names (headers)
-# print(chipo.columns)
-
-print(chipo.apply(pd.value_counts))
-
-# what item was ordered the most?
-# organizes the data
-melted_chipo = pd.melt(chipo, ['item_name'])
+# EXERCISE: what item was ordered the most?
 
 # gets the count of how many times a value occurred in the 'item name' column
+# melted_chipo = pd.melt(chipo, ['item_name'])
 # most = melted_chipo['item_name'].value_counts()
 # the above gave the incorrect response...why?
 
-# solution
+# SOLUTION
 c = chipo.groupby('item_name')
 c = c.sum()
 c = c.sort_values(['quantity'], ascending=False)
 print(c.head(1))
 
-# print(c)
-print('\n')
+# EXERCISE: How many total items were ordered?
+# .quantity returns a dataframe of the each rows and *the num of values in each row?*
+# .sum() sums up the values from the second column from the previous data frame
 print(chipo.quantity.sum())
 
-# Turn the item price into a float (what is this?)
+# EXERCISE: Turn the item price into a float (LOOK INTO THIS DEEPLY)
 dollarizer = lambda x: float(x[1:-1])
 chipo.item_price = chipo.item_price.apply(dollarizer)
 
-# total revenue!
+# EXERCISE: total revenue!
 total_rev = (chipo['item_price'] * chipo['quantity']).sum()
 print 'Revenue was $' + str(total_rev)
 
-# how many orders were made?
-
-# returns the order ids and how many times they appeared
+# EXERCISE: how many orders were made?
+# .count() returns Series with number of non-NA/null observations over requested axis
+# value_count() returns object containing counts of unique values
 total_orders = chipo['order_id'].value_counts().count()
 
 print str(total_orders) + " total orders were made. Nice!"
 
-# What is the average amount per order
+# EXERCISE: What is the average amount per order
 # Total revenue / total orders = average amount per order
-# the below is actually wrong
-avg_per_order = total_rev / total_orders
+# avg_per_order = total_rev / total_orders
+# print 'Average amount per order was $' + str((np.round(avg_per_order, 2)))
+# the above is actually wrong because 
 
-print '\n'
-# print 'Average amount per order was $' + str((np.round(avg_per_order, 2))) + ". No it's not."
-
+# SOLUTION
 order_grouped = chipo.groupby(by=['order_id']).sum()
-
 print order_grouped.mean().item_price
 
-# How many different items are sold?
+# EXERCISE: How many different items are sold?
 
 # items = chipo.groupby(by=['item_name']).values_count()
+# Why does the above not work?
+# The above returns
 
 items = chipo['item_name'].value_counts().count()
-
 print items
 
 
